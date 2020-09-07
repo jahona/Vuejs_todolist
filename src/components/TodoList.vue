dc2b32f9fa1045650027f050813619f4d4ba32b6
정보혁 <jahona@incross.com>
2020-09-07 18:28:22 +0900

    apply css effect using transition-group

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(todoItem, index) in propsdata" :key="todoItem" class="shadow"> 
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
         {{ todoItem }} 
@@ -8,7 +8,7 @@
           <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </section>
 </template>
 
@@ -23,7 +23,7 @@ export default {
 }
 </script>
 
-<style>
+<style ㄴ채ㅔㄷㅇ>
   ul {
     list-style-type: none;
     padding-left: 0px;
@@ -51,5 +51,14 @@ export default {
   .removeBtn {
     margin-left: auto;
     color: #de4343;
+  }
+
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
   }
 </style>
